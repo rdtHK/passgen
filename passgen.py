f8ec71d040d2dcca9b0b5e34b518285d1b702c09
rdtHK <mariop.36754@gmail.com>
2015-10-22 04:47:29 -0200

    PEP8

--- passgen.py
+++ passgen.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # TODO: Add optional master password argument
 # TODO: Better help.
-
-import string, hashlib, argparse, getpass
+import string
+import hashlib
+import argparse
+import getpass
 
 DEFAULT_ALLOWED_CHARS = string.ascii_letters + string.digits
 DEFAULT_PASSWORD_LENGTH = 12
 
+
 def derive_password(master, website, allowed_chars, length):
-    hash_bytes = hashlib.pbkdf2_hmac(
-        'sha256',
-        bytes(master, 'utf-8'),
-        bytes(website + str(length), 'utf-8'),
-        100000)
+    hash_bytes = hashlib.pbkdf2_hmac('sha256', bytes(master, 'utf-8'),
+                                     bytes(website + str(length), 'utf-8'),
+                                     100000)
+    # TODO: Use itertools to pretty this up
     password = ''
     i = 0
     while len(password) < length:
@@ -21,17 +23,13 @@ def derive_password(master, website, allowed_chars, length):
     return password
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--allowed',
-    default=DEFAULT_ALLOWED_CHARS,
-    help='list of allowed characters.'
-)
-parser.add_argument('--length',
-    default=DEFAULT_PASSWORD_LENGTH,
-    type=int,
-    help='password length.'
-)
+parser.add_argument('--allowed', default=DEFAULT_ALLOWED_CHARS,
+                    help='list of allowed characters.')
+parser.add_argument('--length', default=DEFAULT_PASSWORD_LENGTH,
+                    type=int, help='password length.')
 parser.add_argument('website')
 
 args = parser.parse_args()
 master_password = getpass.getpass(prompt='Master password:')
-print(derive_password(master_password, args.website, args.allowed, args.length))
+print(derive_password(master_password, args.website, args.allowed,
+                      args.length))
